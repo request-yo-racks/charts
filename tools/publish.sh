d6c79659a9ffcbf8862e77fae5f70a19e7774f05
Rémy Greinhofer <rgreinhofer@rexchange.com>
2018-02-28 14:48:25 -0600

    Add push target to Makefile
    * Externalize script

---
+++ tools/publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -exuo pipefail
+
+# Go back to the root of the project and checkout gh-pages.
+GROOT=$(git rev-parse --show-toplevel)
+cd "${GROOT}"
+git checkout -b gh-pages origin/gh-pages || git checkout gh-pages
+
+# Add the new packages.
+git add .
+FILES=$(git status -s) 
+git commit -am "Publish charts" -m "${FILES}"
+
+# Push the branch.
+# git push origin gh-pages
+
+# Reindex
+helm repo index --url https://request-yo-racks.github.io/charts .
+
+# Return to the master.
+git checkout master
