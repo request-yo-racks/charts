df9a80a450d53dd759187102060f445cba5bfb19
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-03-11 12:23:10 -0500

    Fix the chart publishing process
    The indexing was happening after committing the new files.

--- tools/publish.sh
+++ tools/publish.sh
@@ -6,16 +6,16 @@ GROOT=$(git rev-parse --show-toplevel)
 cd "${GROOT}"
 git checkout -b gh-pages origin/gh-pages || git checkout gh-pages
 
+# Reindex
+helm repo index --url https://request-yo-racks.github.io/charts .
+
 # Add the new packages.
 git add .
-FILES=$(git status -s) 
+FILES=$(git status -s)
 git commit -am "Publish charts" -m "${FILES}"
 
 # Push the branch.
-# git push origin gh-pages
-
-# Reindex
-helm repo index --url https://request-yo-racks.github.io/charts .
+git push origin gh-pages
 
 # Return to the master.
 git checkout master
