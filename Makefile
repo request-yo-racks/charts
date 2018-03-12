fe28a3af770b78b4d9b32a5e0db17ed3253d69dc
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-03-11 19:06:38 -0500

    Add logic to automatically publish new/updated charts (#14)
    Updates makefiles and add a script to handle the publishing  part of the
    workflow.
    
    Adjusts the CircleCI configuration to support this workflow.

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@
 SHELL = /bin/bash
 TOPDIR = $(shell git rev-parse --show-toplevel)
 
+CHARTS = $(shell ls charts)
+TOPTARGETS = ci-linters dist
+
 help: # Display help
 	@awk -F ':|##' \
 		'/^[^\t].+?:.*?##/ {\
@@ -10,11 +13,20 @@ help: # Display help
 
 default: setup
 
-ci-linters:  ## Run the static analyzers
-	@helm lint --strict charts/*
+bootstrap: ## Install system tools
+	@helm version -c || brew cask install kubernetes-helm
 
 clean: ## Remove unwanted files in this project (!DESTRUCTIVE!)
 	@cd $(TOPDIR) && git clean -ffdx && git reset --hard
 
+publish: ## Publish the charts
+	@bash $(TOPDIR)/tools/publish.sh
+
 setup: ## Setup the full environment (default)
-	@helm version -c || brew cask install kubernetes-helm
+
+$(TOPTARGETS): $(CHARTS)
+
+$(CHARTS):
+	@$(MAKE) -C charts/$@ $(MAKECMDGOALS)
+
+.PHONY: bootstrap clean publish setup $(CHARTS) $(TOPTARGETS)
--- tools/publish.sh
+++ tools/publish.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
-set -exuo pipefail
+set -euo pipefail
 
-# Go back to the root of the project and checkout gh-pages.
+# Save the current branch.
+PREVIOUS_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Return to the previous branch.
+function previous_branch {
+  git checkout "${PREVIOUS_BRANCH}"
+}
+
+# Go back to the root of the project
 GROOT=$(git rev-parse --show-toplevel)
 cd "${GROOT}"
-git checkout -b gh-pages origin/gh-pages || git checkout gh-pages
+
+# Checkout gh-pages.
+git checkout -b gh-pages origin/gh-pages 2>/dev/null || git checkout gh-pages
+
+# Check for new packages.
+if [ -d dist ]; then
+  echo "Nothing to publish."
+  previous_branch
+  exit 0
+fi
+
+NEW_PACKAGES=0
+for f in $(ls dist/*); do
+  if [ ! -f "basename ${f}" ]; then
+    cp "${f}" .
+    NEW_PACKAGES=1
+  fi
+done
+rm -fr ./dist
+
+if [ "${NEW_PACKAGES}" -eq 0 ]; then
+  echo "Nothing new to publish."
+  previous_branch
+  exit 0
+fi
 
 # Reindex
 helm repo index --url https://request-yo-racks.github.io/charts .
@@ -17,5 +49,5 @@ git commit -am "Publish charts" -m "${FILES}"
 # Push the branch.
 git push origin gh-pages
 
-# Return to the master.
-git checkout master
+# Return to the previous branch.
+previous_branch
